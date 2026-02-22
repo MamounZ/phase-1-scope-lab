4b5e12bbc6e0128270c399e23bcdd36b7082eb96
Mamoun Zaid <mam.j.zeid@gmail.com>
2026-02-22 22:28:30 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob'
+const leastFavoriteCustomer = "any";
+
+function upperCaseCustomerName()
+{
+	customerName =  customerName.toUpperCase()
+}
+// console.log(upperCaseCustomerName())
+
+function setBestCustomer()
+{
+	bestCustomer = 'not bob'
+}
+function overwriteBestCustomer(name)
+{
+	bestCustomer = name
+}
+function changeLeastFavoriteCustomer(name)
+{
+	leastFavoriteCustomer = name
+}
